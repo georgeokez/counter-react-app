11c58377e63bb1f783eb5c845c629c2df35e9cc7
Okereka George <georgeokez@gmail.com>
2020-10-16 12:12:24 +0100

    refactored code base to use function components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,122 +1,62 @@
-import React, { Component } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  // eslint-disable-next-line no-unused-vars
-  faTrash,
   faTrashAlt,
   faPlusCircle,
   faMinusCircle,
 } from "@fortawesome/free-solid-svg-icons";
 
-class Counter extends Component {
-  /* 
-  *** State has been lifted to parent component
-  ** State if removed and handled by the parent component (Counter Component)
-  state = {
-    value: this.props.counter.value,
-    tag: []
-  };
-  */
-
-  /*
-  * The constructor
-   constructor() {
-     super();
-     this.handleIncrement = this.handleIncrement.bind(this);
-   }
-  */
-
-  /*
-  *Increment method would be handled by Top level component
-  handleIncrement = currentCount => {
-    console.log(currentCount);
-    this.setState({ value: this.state.value + 1 });
-  };
-  */
-
-  /*
-  *Decrement method would be handled by Top level component
-  handleDecrement = currentCount => {
-    console.log(currentCount);
-    if (this.state.value !== 0) this.setState({ value: this.state.value - 1 });
-  };
-  */
-
-  /*
-  * Helper Method
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: this.state.value + 1 });
-  };
-  */
-
-  render() {
-    return (
-      <div className="m-1 p-1 row">
-        <div className="col-1">
-          <span
-            className={this.getBadgeClasses()}
-            style={{ fontSize: 24, marginLeft: 2 }}
-          >
-            {this.formatCount()}
-          </span>
-        </div>
-
-        {/* Increment button */}
-        <div className="col-md-4" style={{ paddingLeft: 4 }}>
-          <button
-            //onClick={this.doHandleIncrement} old way of passing arguments
-            onClick={() => this.props.onIncrement(this.props.counter)}
-            className="btn btn-primary btn-md m-2"
-          >
-            <FontAwesomeIcon icon={faPlusCircle} />
-          </button>
-
-          {/* Decrement button */}
-          <button
-            onClick={() => this.props.onDecrement(this.props.counter)}
-            className="btn btn-secondary btn-md"
-          >
-            <FontAwesomeIcon icon={faMinusCircle} />
-          </button>
-
-          {/* Delete button */}
-          <button
-            className="btn btn-danger btn-md m-2"
-            onClick={() => this.props.onDelete(this.props.counter.id)}
-          >
-            <FontAwesomeIcon icon={faTrashAlt} />
-          </button>
-        </div>
-
-        {/* {this.renderTag()} */}
-      </div>
-    );
-  }
-
-  renderTag() {
-    if (this.state.tag.length >= 1) {
-      return (
-        <ul>
-          {this.state.tag.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      );
-    } else {
-      return <p>No Tags to display</p>;
-    }
-  }
-
-  getBadgeClasses() {
+const Counter = (props) => {
+  const getBadgeClasses = () => {
     let classes = "badge m-2 Span-label badge-";
-    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    classes += props.counter.value === 0 ? "warning" : "primary";
     return classes;
-  }
+  };
 
-  formatCount() {
-    let count = this.props.counter.value;
+  const formatCount = () => {
+    let count = props.counter.value;
     return count === 0 ? "Zero" : count;
-  }
-}
+  };
+
+  return (
+    <div className="m-1 p-1 row">
+      <div className="col-1">
+        <span
+          className={getBadgeClasses()}
+          style={{ fontSize: 24, marginLeft: 2 }}
+        >
+          {formatCount()}
+        </span>
+      </div>
+
+      {/* Increment button */}
+      <div className="col-md-4" style={{ paddingLeft: 4 }}>
+        <button
+          //onClick={this.doHandleIncrement} old way of passing arguments
+          onClick={() => props.onIncrement(props.counter)}
+          className="btn btn-primary btn-md m-2"
+        >
+          <FontAwesomeIcon icon={faPlusCircle} />
+        </button>
+
+        {/* Decrement button */}
+        <button
+          onClick={() => props.onDecrement(props.counter)}
+          className="btn btn-secondary btn-md"
+        >
+          <FontAwesomeIcon icon={faMinusCircle} />
+        </button>
+
+        {/* Delete button */}
+        <button
+          className="btn btn-danger btn-md m-2"
+          onClick={() => props.onDelete(props.counter.id)}
+        >
+          <FontAwesomeIcon icon={faTrashAlt} />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Counter;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,36 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
 // NavBar Component
-class NavBar extends Component {
-  state = {
-    init: 1,
-  };
-
-  getCountValue = () => {
-    if (this.state.init === 1) {
-      this.setState((state) => state.init++);
-      return this.props.initCountValue;
-    } else {
-      return this.props.totalValue;
-    }
-  };
-
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="/">
-          Counter{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-          <span className="m-2">TotalCount</span>
-          <span className="badge badge-pill badge-primary">
-            {this.getCountValue()}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+const NavBar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        Counter{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+        <span className="m-2">TotalCount</span>
+        <span className="badge badge-pill badge-primary">
+          {props.totalValue}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
